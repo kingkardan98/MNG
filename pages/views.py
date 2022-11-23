da5f6e14dce5c377e3db3b4948dea39cdf592f12
Francesco <fr.bambina@gmail.com>
2022-11-23 23:40:53 +0100

    Get views from HTML file instead of manually writing them in the views.py file

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils import read_html
 
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("<h1> This is MNG </h1>")
+    html = read_html('home_view.html')
+    return HttpResponse(html, status=200)
+
+def contact_view(request):
+    html = read_html('contact_view.html')
+    return HttpResponse(html, status=200)
