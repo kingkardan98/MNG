bef59e36dc609931150d53ece5cc0295a9e99341
Francesco <fr.bambina@gmail.com>
2022-11-23 22:23:33 +0100

    Delete __pycache__

--- user/models.py
+++ user/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.TextField()
-    email = models.EmailField()
-    password = models.TextField()
+    username = models.CharField(max_length=256)
+    email = models.EmailField(max_length=256)
+    password = models.CharField(max_length=256)
 
-    moneyAvailability = models.FloatField()
-    spendingLimit = models.FloatField()
+    moneyAvailability = models.DecimalField(max_digits=10000, decimal_places=2)
+    spendingLimit = models.DecimalField(max_digits=10000, decimal_places=2)
