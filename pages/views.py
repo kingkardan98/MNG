6fff05156753484bbba5fe0161841158fc79e92e
Francesco <fr.bambina@gmail.com>
2022-11-24 16:11:36 +0100

    Use render instead of homebrew read_html function

--- pages/views.py
+++ pages/views.py
@@ -5,9 +5,7 @@ from utils import read_html
 # Create your views here.
 
 def home_view(request):
-    html = read_html('home_view.html')
-    return render(request, "html_views/home_view.html", {})
+    return render(request, "home_view.html", {})
 
 def contacts_view(request):
-    html = read_html('contacts_view.html')
-    return HttpResponse(html, status=200)
+    return render(request, "contacts_view.html", {})
