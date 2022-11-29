17bbe50b5b46972b829d0bc7a3a25521bab76e3b
Francesco <fr.bambina@gmail.com>
2022-11-29 17:46:31 +0100

    Create detail user view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+def user_detail_view(request):
+    obj = User.objects.get(id=1)
+    context = {
+        "Username": obj.username,
+        "Email": obj.email,
+        "Money availability": obj.moneyAvailability,
+        "Spending limit": obj.spendingLimit
+    }
+
+    return render(request, "user/detail.html", context)
