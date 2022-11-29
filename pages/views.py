420ec6479901a5b66fe449af24b70b1d4b07d8e5
Francesco <fr.bambina@gmail.com>
2022-11-29 17:30:03 +0100

    Restructure template path

--- pages/views.py
+++ pages/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return render(request, "home_view.html", {})
+    return render(request, "html/home_view.html", {})
 
 def contacts_view(request):
     context = {
         "contacts_text": "Contact information",
         "contacts_dict": {"mary": "123", "jackson": "000", "meeseeks": "4446064447777833777063377773333557777"} 
     }
-    return render(request, "contacts_view.html", context)
+    return render(request, "html/contacts_view.html", context)
