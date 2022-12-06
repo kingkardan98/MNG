7796576bf09973561c42e46edec5d268a2afa0f2
Francesco Bambina <francescobambina@192.168.1.62>
2022-12-07 00:55:05 +0100

    Add check for minimum password length, updated database entries accordingly

--- user/models.py
+++ user/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=256)
     email = models.EmailField(max_length=256)
-    password = models.CharField(max_length=512)
+    password = models.CharField(max_length=512, validators=[MinLengthValidator(8)])
 
     availability = models.DecimalField(max_digits=10000, decimal_places=2)
     spendable = models.DecimalField(max_digits=10000, decimal_places=2, db_column='spending')
