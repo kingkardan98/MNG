8e9bb4eeaac18466ed80c5300d0e672ac0806d08
Francesco Bambina <francescobambina@192.168.1.62>
2022-12-08 10:46:10 +0100

    Minor line changes

--- user/forms.py
+++ user/forms.py
@@ -83,7 +83,7 @@ class UserForm(forms.ModelForm):
             self.add_error('confirm_password',
                 "Passwords don't match."
             )
-
+            
         if spendable > availability:
             self.add_error('spendable',
                 "Spending limit can't be higher than availability."
