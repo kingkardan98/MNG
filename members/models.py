361f8d24365598bf06ac4506d840b2bd05eb9925
Francesco <fr.bambina@gmail.com>
2022-12-24 23:04:06 +0100

    Removed member password

--- members/models.py
+++ members/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 class Member(models.Model):
     name = models.CharField(max_length=256, validators=[MinLengthValidator(8)])
     email = models.EmailField(max_length=256)
-    password = models.CharField(max_length=512, validators=[MinLengthValidator(8)])
 
     availability = models.DecimalField(max_digits=10000, decimal_places=2)
     spendable = models.DecimalField(max_digits=10000, decimal_places=2, db_column='spending')
