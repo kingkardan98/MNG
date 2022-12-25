630405353bc77b5c8bf5a182c3559a5f93266a09
Francesco <fr.bambina@gmail.com>
2022-12-25 03:18:18 +0100

    Delete redundant functions

--- members/member_create_data_check.py
+++ members/member_create_data_check.py
@@ -1,49 +1,5 @@
 import string
 
-def passwordMatch(data):
-    password = data['password']
-    confirm_password = data['confirm_password']
-
-    if password != confirm_password:
-        return 'unmatch'
-    return ''
-
-def hasSpecialChar(data):
-    password = data['password']
-    charList = ['@', '#', '!', '?', '=', '_', '-', '+', '*', '(', ')', '[', ']', '{', '}', '£', '$', '%', '&', '^']
-
-    for char in password:
-        if char in charList:
-            return ''
-    return 'special'
-
-def hasDigit(data):
-    password = data['password']
-    numList = string.digits
-
-    for char in password:
-        if char in numList:
-            return ''
-    return 'digit'
-
-def hasLowercase(data):
-    password = data['password']
-    lowerList = string.ascii_lowercase
-
-    for char in password:
-        if char in lowerList:
-            return ''
-    return 'lowercase'
-
-def hasUppercase(data):
-    password = data['password']
-    upperList = string.ascii_uppercase
-
-    for char in password:
-        if char in upperList:
-            return ''
-    return 'uppercase'
-
 def spendableCheck(data):
     spendable = data['spendable']
     availability = data['availability']
@@ -52,19 +8,9 @@ def spendableCheck(data):
         return 'spendable'
     return ''
 
-checkFuncList = [passwordMatch, hasSpecialChar, hasDigit, hasLowercase, hasUppercase, spendableCheck]
-errorMessages = {'unmatch': "Passwords don't match.", 
-                 'special': "Password must contain at least a special character.",
-                 'digit': "Password must contain at least one digit.",
-                 'lowercase': "Password must contain at least a lowercase letter.",
-                 'uppercase': "Password must contain at least an uppercase letter.",
-                 'spendable': "Spending limit must be lower than availability."}
+checkFuncList = [spendableCheck]
+errorMessages = {'spendable': "Spending limit must be lower than availability."}
 relations = {
-    'unmatch': 'confirm_password',
-    'special': 'password',
-    'digit': 'password',
-    'lowercase': 'password',
-    'uppercase': 'password',
     'spendable': 'spendable'
 
 }
