fbcff071b5613f75f2c788295d160c6ac68eccb9
Francesco Bambina <francescobambina@192.168.1.62>
2023-01-05 01:40:32 +0100

    Fix comments, add edit history and user deletion urls

--- MNG/urls.py
+++ MNG/urls.py
@@ -30,11 +30,12 @@ urlpatterns = [
     path('about/', about_view, name='about_view'),                                              # pages.views
     path('refused/', refused, name='refused'),                                                  # pages.views
 
-    path('create_member', member_create_view, name='member_create_view'),                       # member.views
-    path('<str:logged_user>/member_list', member_list_view, name="member_list_view"),           # member.views
-    path('user/<str:name>', member_detail_view, name='member_detail_view'),                     # member.views
-    path('user/<str:name>/delete', delete_member_view, name='delete_member_view'),              # member.views
-    path('user/<str:name>/update', update_member_view, name='update_member_view'),              # member.views
+    path('create_member/', member_create_view, name='member_create_view'),                       # member.views
+    path('<str:logged_user>/member_list/', member_list_view, name="member_list_view"),           # member.views
+    path('user/<str:name>/', member_detail_view, name='member_detail_view'),                     # member.views
+    path('user/<str:name>/history/', member_history_list_view, name='member_history_list_view'), # member.views
+    path('user/<str:name>/delete/', delete_member_view, name='delete_member_view'),              # member.views
+    path('user/<str:name>/update/', update_member_view, name='update_member_view'),              # member.views
     path('creation_success/', create_success_view, name='create_success_view'),                 # member.views
     path('update_success/', update_success_view, name='update_success_view'),                   # member.views
     path('delete_success/', delete_success_view, name='delete_success_view'),                   # member.views
@@ -42,5 +43,7 @@ urlpatterns = [
     path('login/', include('django.contrib.auth.urls')),                                        # userAuth.views
     path('login/', login_user, name='login_user'),                                              # userAuth.views
     path('logout/', logout_user, name='logout_user'),                                           # userAuth.views
-    path('create_user', create_user_view, name = 'create_user_view')                            # userAuth.views
+    path('create_user/', create_user_view, name = 'create_user_view'),                           # userAuth.views
+    path('delete/', delete_user, name='delete_user')                              # userAuth.views
+
 ]
