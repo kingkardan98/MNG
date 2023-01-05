89a9f27e39bfae3c5a85435282f2c562ec2b669e
Francesco Bambina <francescobambina@192.168.1.62>
2023-01-05 01:39:13 +0100

    Create and register admin history class

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from .models import Member
-# Register your models here.
+from django.utils.html import format_html
 
-admin.site.register(Member)
+class MemberHistoryAdmin(SimpleHistoryAdmin):
+    history_list_display = ["changed_fields","list_changes"]
+    
+    def changed_fields(self, obj):
+        if obj.prev_record:
+            delta = obj.diff_against(obj.prev_record)
+            return delta.changed_fields
+        return None
+
+    def list_changes(self, obj):
+        fields = ""
+        if obj.prev_record:
+            delta = obj.diff_against(obj.prev_record)
+
+            for change in delta.changes:
+                fields += "%s changed from %s to %s." % (change.field, change.old, change.new)
+            return fields
+        return None
+
+
+admin.site.register(Member, MemberHistoryAdmin)
