a117660c5b0e1f746bd1d35932f3645d54860acd
Francesco Bambina <francescobambina@192.168.1.62>
2023-01-08 02:27:47 +0100

    Make comments line up

--- MNG/urls.py
+++ MNG/urls.py
@@ -30,29 +30,30 @@ urlpatterns = [
 
     # All English URL patterns
 
-    path('en', home_view, name='home'),                                                                   # pages.views
-    path('en/contacts/', contacts_view, name='contacts'),                                                 # pages.views
-    path('en/about/', about_view, name='about_view'),                                                     # pages.views
-    path('en/refused/', refused, name='refused'),                                                         # pages.views
+    path('en/', home_view, name='home'),                                                                        # pages.views
+    path('en/contacts/', contacts_view, name='contacts'),                                                       # pages.views
+    path('en/about/', about_view, name='about_view'),                                                           # pages.views
+    path('en/refused/', refused, name='refused'),                                                               # pages.views
 
-    path('en/create_member/', member_create_view, name='member_create_view'),                             # member.views
-    path('en/<str:logged_user>/member_list/', member_list_view, name="member_list_view"),                 # member.views
-    path('en/user/<str:name>/', member_detail_view, name='member_detail_view'),                           # member.views
-    path('en/user/<str:name>/history/', member_history_list_view, name='member_history_list_view'),       # member.views
-    path('en/user/<str:name>/delete/', delete_member_view, name='delete_member_view'),                    # member.views
-    path('en/user/<str:name>/update/', update_member_view, name='update_member_view'),                    # member.views
-    path('en/creation_success/', create_success_view, name='create_success_view'),                        # member.views
-    path('en/update_success/', update_success_view, name='update_success_view'),                          # member.views
-    path('en/delete_success/', delete_success_view, name='delete_success_view'),                          # member.views
+    path('en/create_member/', member_create_view, name='member_create_view'),                                   # member.views
+    path('en/<str:logged_user>/member_list/', member_list_view, name="member_list_view"),                       # member.views
+    path('en/user/<str:name>/', member_detail_view, name='member_detail_view'),                                 # member.views
+    path('en/user/<str:name>/history/', member_history_list_view, name='member_history_list_view'),             # member.views
+    path('en/user/<str:name>/delete/', delete_member_view, name='delete_member_view'),                          # member.views
+    path('en/user/<str:name>/update/', update_member_view, name='update_member_view'),                          # member.views
+    path('en/creation_success/', create_success_view, name='create_success_view'),                              # member.views
+    path('en/update_success/', update_success_view, name='update_success_view'),                                # member.views
+    path('en/delete_success/', delete_success_view, name='delete_success_view'),                                # member.views
 
-    path('login/', include('django.contrib.auth.urls')),                                                  # userAuth.views
-    path('login/', login_user, name='login_user'),                                                        # userAuth.views
-    path('en/logout/', logout_user, name='logout_user'),                                                  # userAuth.views
-    path('en/create_user/', create_user_view, name = 'create_user_view'),                                 # userAuth.views
-    path('en/delete/', delete_user, name='delete_user'),                                                  # userAuth.views
+    path('login/', include('django.contrib.auth.urls')),                                                        # userAuth.views
+    path('login/', login_user, name='login_user'),                                                              # userAuth.views
+    path('en/logout/', logout_user, name='logout_user'),                                                        # userAuth.views
+    path('en/create_user/', create_user_view, name = 'create_user_view'),                                       # userAuth.views
+    path('en/delete/', delete_user, name='delete_user'),                                                        # userAuth.views
 
     # All Italian URL Patterns
-    path('it', home_view_it, name='home_view_it'),                                                               # pages.views
+    
+    path('it', home_view_it, name='home_view_it'),                                                              # pages.views
     path('it/contacts/', contacts_view_it, name='contacts_it'),                                                 # pages.views
     path('it/about/', about_view_it, name='about_view_it'),                                                     # pages.views
     path('it/refused/', refused_it, name='refused_it'),                                                         # pages.views
